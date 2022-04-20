79bd354154087907b56a963b846cb31d396b9ae2
nikesh pathania <nikesh.pathania@gmail.com>
2022-04-20 07:22:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(){
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 50) {
+        document.getElementById('navbar_top').classList.add('navbar_top');
+        // add padding top to show content behind navbar
+        navbar_height = document.querySelector('.navbar').offsetHeight;
+        document.body.style.paddingTop = navbar_height + 'px';
+      } else {
+        document.getElementById('navbar_top').classList.remove('navbar_top');
+        // remove padding top from body
+        document.body.style.paddingTop = '0';
+      } 
+  });
+}); 
+// DOMContentLoaded  end
